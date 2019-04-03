a944188a65ea7baa12f6503ae9d701430b70f529
cpotdevin <chrpotdevin@gmail.com>
2019-04-02 23:07:07 -0500

    completar retos

--- src/components/job.js
+++ src/components/job.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import { FormattedDate, FormattedRelative, FormattedNumber, FormattedMessage } from 'react-intl';
 
 export default class Job extends React.Component {
-
   	render() {
   		return (
   			<tr>
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+  					<td>{this.props.offer.company}</td>
+						<td>
+							{this.props.offer.salary} {Math.floor(this.props.offer.salary) === 1 ? (
+								<FormattedMessage id="Million" />
+							) : (
+								<FormattedMessage id="Millions" />
+							)}
+						</td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+							<FormattedDate
+								value={new Date(this.props.offer.date)}
+								year='numeric'
+								month='long'
+								day='numeric'
+								weekday='long'
+							/> (<FormattedRelative value={this.props.offer.date}/>)
+						</td>
+						<td>
+							<FormattedNumber value={this.props.offer.views} />
+						</td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider, addLocaleData } from 'react-intl';
 
 import JobsList from "./components/jobsList";
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEsMessages from './locales/es';
+import enLocaleData from 'react-intl/locale-data/en';
+import localeEnMessages from './locales/en';
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData([...enLocaleData, ...esLocaleData]);
+
+const messages = {
+  'en': localeEnMessages,
+  'es': localeEsMessages
+}
+
+const language = navigator.language.split('-')[0];
+
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages[language]}>
+    <JobsList/>
+  </IntlProvider>,
+  document.getElementById("root")
+);
